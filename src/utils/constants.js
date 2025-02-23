885864c3d36a8dc54a35f4b802fb8a697d301f5c
Donald Johnson <djjohnson380@gmail.com>
2025-02-22 22:25:39 -0700

    Update on Project 10 after Part 12

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 33.30616,
+  longitude: -111.841248,
+};
+
+export const APIkey = "093aba1ad80da9ed61da02536692100e";
